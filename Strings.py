33d254b6e67da2e1e0c85c2505381fe85dec957c
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-09-18 18:00:17 +0530

    Strings

---
+++ Strings.py
@@ -0,0 +1,13 @@
+#Here we will se how to work with strings in python
+s=input("Enter Your first Name: ")
+# l=input("Enter ypu Lastname: ")
+
+# print("Your full name : ",s+" "+l)
+
+# #strings in python can be used as character array
+
+# print("First charatacter of firstname is : ",s[0])
+
+#inorder to print each character
+for i in s:
+    print(i)
