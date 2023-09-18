3c677e35184983e3d805c53a989ac7d54cfe9dce
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-09-18 15:29:43 +0530

    Input

---
+++ Input.py
@@ -0,0 +1,13 @@
+print("Today we will learn how to take input from user in Python")
+
+#to take input we need to store it in the variable
+
+a=input("Enter your name : ")
+#this will by default take input in format of a string
+
+print("Oh, hi ", a)
+#Lets add few of the input 
+
+b=int(input("Enter a Number :"))
+c= int(input("Enter another number :"))
+print("The sum of the above two numbers  = ",b+c)
