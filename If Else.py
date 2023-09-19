46269b46738b6844c2b07e3438b41aed6f45366e
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-09-19 23:28:28 +0530

    if else elif

---
+++ If Else.py
@@ -0,0 +1,8 @@
+#this will contain conditional statements
+a=int(input("Enter your Age: "))
+if(a<18):
+    print("Your still have some time to become an adult")
+elif(a>100):
+    print("Keep your hands away from steering")
+else:
+    print("You can drive any vehicle")
