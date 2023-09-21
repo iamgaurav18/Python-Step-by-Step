5d0d3e5f2a4340f00811bd6dd210269573b233cf
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-09-21 08:12:16 +0530

    while and Do while

--- 9. While loop.py
+++ 9. While loop.py
@@ -4,4 +4,13 @@ while(a<5):
     print(a,end=" ")
     a+=1
 else:
-    print("We are out of ehilr loop")
+    print("We are out of while loop")
+
+# how to emulate do while loop
+
+while True:
+    a=int(input("print a number greater than 10 : "))
+    if(a>10):
+        break
+    else:
+        continue
