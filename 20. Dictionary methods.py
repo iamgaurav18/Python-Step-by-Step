1b9827968e218017d4266ec3abe21dfbc1802e7d
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-09-28 00:07:30 +0530

    Ditcionary methods

--- 20. Dictionary methods.py
+++ 20. Dictionary methods.py
@@ -1,14 +1,18 @@
 d={1:23,2:34,3:45}
 e={5:56}
 
-d.update(e)
+d.update(e)  # this will add key values at the end of the dict
 print(d)
 
-e.clear()
+e.clear()   # this will clear the entire dictionary
 print(e)
 
-d.pop(1)
+d.pop(1)    #this will delete the specified item with its value from dict
 print(d)
 
-d.popitem()
+d.popitem()  #this will delete the last item
 print(d)
+
+del e         #this will delete entire dict
+
+del d[2]       #this will also delete specified key with its value
