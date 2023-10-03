0862abf8f9cc6230cf131b5b077745b7763e5f81
Gaurav Vishwakarma <gaurav25vishwakarma@gmail.com>
2023-10-03 23:53:53 +0530

    __name__

---
+++ 29. __name__.py
@@ -0,0 +1,6 @@
+# __name__ is by default set to the name of the current module. for example
+print(__name__)
+#this will return name of the current module that is __main__
+
+# this same code will return name of the doe in which it is used
+# comes in handy when importing some modules and dont want to execute unwanted code from that module (print statements)
